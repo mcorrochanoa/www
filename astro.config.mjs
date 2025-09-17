ef79d014f6d014190f5d40bfe278bfbc62815d25
Marcos Corrochano Arroyo <mcorrochanoa@outlook.com>
2025-09-17 10:33:36 +0200

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  adapter: vercel(),
+  integrations: [preact(), sitemap()]
+});
