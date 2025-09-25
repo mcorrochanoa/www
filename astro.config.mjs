b0fe09ff6089f029cd758ad81124c8f15f72e17b
Marcos Corrochano Arroyo <mcorrochanoa@outlook.com>
2025-09-25 23:48:07 +0200

    format files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   adapter: vercel(),
-  integrations: [preact(), sitemap()],
-});
+  integrations: [preact(), sitemap()]
+})
